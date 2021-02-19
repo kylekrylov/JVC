7f535f5bccf014cbf47b2547856790c3180cf78b
kylekylerylov <kylekrylov@gmail.com>
2021-02-19 18:31:15 +0300

    //

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,35 @@
 const accordion = document.getElementsByClassName('item-benefits-list');
-for (i = 0 ; i<accordion.length; i++){
-   accordion[i].addEventListener('click',function(){
+for (i = 0; i < accordion.length; i++) {
+   accordion[i].addEventListener('click', function () {
       this.classList.toggle('active')
    })
 };
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
 for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
+   smoothLink.addEventListener('click', function (e) {
+      e.preventDefault();
+      const id = smoothLink.getAttribute('href');
 
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
+      document.querySelector(id).scrollIntoView({
+         behavior: 'smooth',
+         block: 'start'
+      });
+   });
 };
-window.onscroll = function() {
+window.onscroll = function () {
    let scrollElem = document.getElementById("scrollToTop");
    if (window.scrollY > document.documentElement.clientHeight) {
       scrollElem.style.opacity = "1";
    } else {
-       scrollElem.style.opacity = "0";
+      scrollElem.style.opacity = "0";
    }
 };
 let timeOut;
 function goUp() {
-   let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-   if(top > 0) {
-      window.scrollBy(0,-100);
-      timeOut = setTimeout('goUp()',10);
+   let top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+   if (top > 0) {
+      window.scrollBy(0, -100);
+      timeOut = setTimeout('goUp()', 10);
    } else clearTimeout(timeOut);
 };
 
@@ -37,9 +37,11 @@ function goUp() {
 //    document.querySelector('.block-jvc__main-img').src = image;
 // }
 
-const images = document.querySelectorAll('.block-jvc__main-img');
-images.forEach( ( image ) => {
-  function changeImage( newImage ){
-    image.src = newImage;
-   }
+const colors = document.querySelectorAll( '.color' );
+
+colors.forEach( ( color ) => {
+  color.addEventListener( 'mouseover', () =>
+  {
+    color.closest( '.block-jvc__main' ).querySelector( '.block-jvc__main-img' ).src = color.getAttribute( 'data-image' );
+  });
 });
